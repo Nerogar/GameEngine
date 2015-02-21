62c35a69752dc76512402457a4ffef7fc6b0482a
Nerogar <nerogar@arcor.de>
2015-02-21 16:57:53 +0100

    new renderQuad method in RenderHelper

--- GameEngine/src/de/nerogar/render/RenderHelper.java
+++ GameEngine/src/de/nerogar/render/RenderHelper.java
@@ -22,4 +22,22 @@ public class RenderHelper {
 		glEnd();
 	}
 
+	public static void renderQuad(ScreenProperties screenProperties, float width, float height, float posX, float posY) {
+		glBegin(GL_QUADS);
+
+		glTexCoord2f(1f, 1f);
+		glVertex3f(screenProperties.getRenderWidth() * (posX + width), screenProperties.getRenderHeight() * (posY + height), -1f);
+
+		glTexCoord2f(0f, 1f);
+		glVertex3f(screenProperties.getRenderWidth() * (posX), screenProperties.getRenderHeight() * (posY + height), -1f);
+
+		glTexCoord2f(0f, 0f);
+		glVertex3f(screenProperties.getRenderWidth() * (posX), screenProperties.getRenderHeight() * (posY), -1f);
+
+		glTexCoord2f(1f, 0f);
+		glVertex3f(screenProperties.getRenderWidth() * (posX + width), screenProperties.getRenderHeight() * (posY), -1f);
+
+		glEnd();
+	}
+
 }
