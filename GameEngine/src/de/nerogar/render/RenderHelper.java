79a4f5cffe5bb2cc7a27d4ea71bd3341e30bf6ef
Nerogar <nerogar@arcor.de>
2015-02-23 19:20:42 +0100

    more render methods in RenderHelper

--- GameEngine/src/de/nerogar/render/RenderHelper.java
+++ GameEngine/src/de/nerogar/render/RenderHelper.java
@@ -4,14 +4,8 @@ import static org.lwjgl.opengl.GL11.*;
 
 public class RenderHelper {
 
-	public static void renderFullscreenQuad(ScreenProperties screenProperties) {
+	public static void renderFBOFullscreenQuad(ScreenProperties screenProperties) {
 		glBegin(GL_QUADS);
-
-		glTexCoord2f(1f, 1f);
-		glVertex3f(screenProperties.getRenderWidth(), screenProperties.getRenderHeight(), -1f);
-
-		glTexCoord2f(0f, 1f);
-		glVertex3f(0f, screenProperties.getRenderHeight(), -1f);
 
 		glTexCoord2f(0f, 0f);
 		glVertex3f(0f, 0f, -1f);
@@ -19,11 +13,41 @@ public class RenderHelper {
 		glTexCoord2f(1f, 0f);
 		glVertex3f(screenProperties.getRenderWidth(), 0f, -1f);
 
+		glTexCoord2f(1f, 1f);
+		glVertex3f(screenProperties.getRenderWidth(), screenProperties.getRenderHeight(), -1f);
+
+		glTexCoord2f(0f, 1f);
+		glVertex3f(0f, screenProperties.getRenderHeight(), -1f);
+
 		glEnd();
 	}
 
-	public static void renderQuad(ScreenProperties screenProperties, float width, float height, float posX, float posY) {
+	public static void renderTextureFullscreenQuad(ScreenProperties screenProperties) {
 		glBegin(GL_QUADS);
+
+		glTexCoord2f(0f, 1f);
+		glVertex3f(0f, 0f, -1f);
+
+		glTexCoord2f(1f, 1f);
+		glVertex3f(screenProperties.getRenderWidth(), 0f, -1f);
+
+		glTexCoord2f(1f, 0f);
+		glVertex3f(screenProperties.getRenderWidth(), screenProperties.getRenderHeight(), -1f);
+
+		glTexCoord2f(0f, 0f);
+		glVertex3f(0f, screenProperties.getRenderHeight(), -1f);
+
+		glEnd();
+	}
+	
+	public static void renderFBOQuad(ScreenProperties screenProperties, float width, float height, float posX, float posY) {
+		glBegin(GL_QUADS);
+
+		glTexCoord2f(0f, 0f);
+		glVertex3f(screenProperties.getRenderWidth() * (posX), screenProperties.getRenderHeight() * (posY), -1f);
+
+		glTexCoord2f(1f, 0f);
+		glVertex3f(screenProperties.getRenderWidth() * (posX + width), screenProperties.getRenderHeight() * (posY), -1f);
 
 		glTexCoord2f(1f, 1f);
 		glVertex3f(screenProperties.getRenderWidth() * (posX + width), screenProperties.getRenderHeight() * (posY + height), -1f);
@@ -31,11 +55,59 @@ public class RenderHelper {
 		glTexCoord2f(0f, 1f);
 		glVertex3f(screenProperties.getRenderWidth() * (posX), screenProperties.getRenderHeight() * (posY + height), -1f);
 
-		glTexCoord2f(0f, 0f);
+		glEnd();
+	}
+
+	public static void renderTextureQuad(ScreenProperties screenProperties, float width, float height, float posX, float posY) {
+		glBegin(GL_QUADS);
+
+		glTexCoord2f(0f, 1f);
 		glVertex3f(screenProperties.getRenderWidth() * (posX), screenProperties.getRenderHeight() * (posY), -1f);
 
-		glTexCoord2f(1f, 0f);
+		glTexCoord2f(1f, 1f);
 		glVertex3f(screenProperties.getRenderWidth() * (posX + width), screenProperties.getRenderHeight() * (posY), -1f);
+
+		glTexCoord2f(1f, 0f);
+		glVertex3f(screenProperties.getRenderWidth() * (posX + width), screenProperties.getRenderHeight() * (posY + height), -1f);
+
+		glTexCoord2f(0f, 0f);
+		glVertex3f(screenProperties.getRenderWidth() * (posX), screenProperties.getRenderHeight() * (posY + height), -1f);
+
+		glEnd();
+	}
+
+	public static void renderFBOQuadAbsolute(float width, float height, float posX, float posY) {
+		glBegin(GL_QUADS);
+
+		glTexCoord2f(0f, 0f);
+		glVertex3f((posX), (posY), -1f);
+
+		glTexCoord2f(1f, 0f);
+		glVertex3f((posX + width), (posY), -1f);
+
+		glTexCoord2f(1f, 1f);
+		glVertex3f((posX + width), (posY + height), -1f);
+
+		glTexCoord2f(0f, 1f);
+		glVertex3f((posX), (posY + height), -1f);
+
+		glEnd();
+	}
+	
+	public static void renderTextureQuadAbsolute(float width, float height, float posX, float posY) {
+		glBegin(GL_QUADS);
+
+		glTexCoord2f(0f, 1f);
+		glVertex3f((posX), (posY), -1f);
+
+		glTexCoord2f(1f, 1f);
+		glVertex3f((posX + width), (posY), -1f);
+
+		glTexCoord2f(1f, 0f);
+		glVertex3f((posX + width), (posY + height), -1f);
+
+		glTexCoord2f(0f, 0f);
+		glVertex3f((posX), (posY + height), -1f);
 
 		glEnd();
 	}
