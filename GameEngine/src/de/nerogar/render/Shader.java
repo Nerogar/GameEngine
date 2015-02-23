00fb3889957a7d8f1f0ff41a89799022f3da8216
Nerogar <nerogar@arcor.de>
2015-02-23 19:20:24 +0100

    uniform cache in shaders

--- GameEngine/src/de/nerogar/render/Shader.java
+++ GameEngine/src/de/nerogar/render/Shader.java
@@ -5,6 +5,8 @@ import static org.lwjgl.opengl.GL20.*;
 
 import java.io.*;
 import java.nio.FloatBuffer;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.lwjgl.BufferUtils;
 
@@ -16,6 +18,8 @@ public class Shader {
 	private String fragmentShaderFile, fragmentShader;
 	private int vertexShaderHandle, fragmentShaderHandle;
 
+	private Map<String, Integer> uniformCache;
+
 	private boolean active;
 	private boolean compiled;
 
@@ -23,11 +27,22 @@ public class Shader {
 		this.vertexShaderFile = vertexShaderFile;
 		this.fragmentShaderFile = fragmentShaderFile;
 
+		uniformCache = new HashMap<String, Integer>();
+
 		reloadFiles();
 		reCompile();
 	}
 
 	//uniforms
+
+	private int getUniformLocation(String name) {
+		Integer location = uniformCache.get(name);
+		if (location != null) return location;
+
+		location = glGetUniformLocation(shaderHandle, name);
+		uniformCache.put(name, location);
+		return location;
+	}
 
 	//float
 	public void setUniformf(String name, float[] values) {
@@ -38,49 +53,49 @@ public class Shader {
 	}
 
 	public void setUniformf(String name, FloatBuffer floatBuffer) {
-		glUniform1(glGetUniformLocation(shaderHandle, name), floatBuffer);
+		glUniform1(getUniformLocation(name), floatBuffer);
 	}
 
 	public void setUniform1f(String name, float f0) {
-		glUniform1f(glGetUniformLocation(shaderHandle, name), f0);
+		glUniform1f(getUniformLocation(name), f0);
 	}
 
 	public void setUniform2f(String name, float f0, float f1) {
-		glUniform2f(glGetUniformLocation(shaderHandle, name), f0, f1);
+		glUniform2f(getUniformLocation(name), f0, f1);
 	}
 
 	public void setUniform3f(String name, float f0, float f1, float f2) {
-		glUniform3f(glGetUniformLocation(shaderHandle, name), f0, f1, f2);
+		glUniform3f(getUniformLocation(name), f0, f1, f2);
 	}
 
 	public void setUniform4f(String name, float f0, float f1, float f2, float f3) {
-		glUniform4f(glGetUniformLocation(shaderHandle, name), f0, f1, f2, f3);
+		glUniform4f(getUniformLocation(name), f0, f1, f2, f3);
 	}
 
 	public void setUniformMat4f(String name, FloatBuffer buffer) {
-		glUniformMatrix4(glGetUniformLocation(shaderHandle, name), true, buffer);
+		glUniformMatrix4(getUniformLocation(name), true, buffer);
 	}
 
 	//int
 	public void setUniform1i(String name, int i0) {
-		glUniform1i(glGetUniformLocation(shaderHandle, name), i0);
+		glUniform1i(getUniformLocation(name), i0);
 	}
 
 	public void setUniform2i(String name, int i0, int i1) {
-		glUniform2i(glGetUniformLocation(shaderHandle, name), i0, i1);
+		glUniform2i(getUniformLocation(name), i0, i1);
 	}
 
 	public void setUniform3i(String name, int i0, int i1, int i2) {
-		glUniform3i(glGetUniformLocation(shaderHandle, name), i0, i1, i2);
+		glUniform3i(getUniformLocation(name), i0, i1, i2);
 	}
 
 	public void setUniform4i(String name, int i0, int i1, int i2, int i3) {
-		glUniform4i(glGetUniformLocation(shaderHandle, name), i0, i1, i2, i3);
+		glUniform4i(getUniformLocation(name), i0, i1, i2, i3);
 	}
 
 	//boolean
 	public void setUniform1bool(String name, boolean b0) {
-		glUniform1i(glGetUniformLocation(shaderHandle, name), b0 ? 1 : 0);
+		glUniform1i(getUniformLocation(name), b0 ? 1 : 0);
 	}
 
 	//end uniforms
@@ -123,12 +138,15 @@ public class Shader {
 				System.err.println(glGetProgramInfoLog(shaderHandle, 1024));
 				compileError = true;
 			}
-			glDeleteShader(vertexShaderHandle);
-			glDeleteShader(fragmentShaderHandle);
 		}
+
+		glDeleteShader(vertexShaderHandle);
+		glDeleteShader(fragmentShaderHandle);
 
 		if (!compileError) {
 			compiled = true;
+		} else {
+			cleanup();
 		}
 	}
 
@@ -172,16 +190,9 @@ public class Shader {
 		}
 	}
 
-	public void setResolution(float x, float y) {
-		activate();
-
-		glUniform2f(glGetUniformLocation(shaderHandle, "resolution"), x, y);
-
-		deactivate();
-	}
-
 	public void cleanup() {
 		glDeleteProgram(shaderHandle);
+		uniformCache.clear();
 		compiled = false;
 	}
 
